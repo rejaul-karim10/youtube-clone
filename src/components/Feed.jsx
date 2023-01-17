0351a5f8afd4134f9f93a90b52213bcfc24e050d
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-01-17 11:28:01 +0800

    added sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React from "react";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar/>
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color:"#fff"}}>
+          Copyright FrontendTeam
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
